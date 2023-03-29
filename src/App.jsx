77d370aedc561b12745a7ab2af015af5dde911b7
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-03-29 14:12:01 +0300

    Made the Accordion component dynamic and added mockdata for it inside the main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,24 @@
-import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
-import Button from "./button.jsx";
+import Accordion from "./components/Accordion.jsx";
 
 function App() {
-  const handleClick = () => {
-    console.log("Click!");
-  };
-  return (
-    <div>
-      <div>
-        <Button success outline rounded className="mb-5" onClick={handleClick}>
-          <GoBell />
-          Click!
-        </Button>
-      </div>
-      <div>
-        <Button warning onMouseOver={handleClick}>
-          <GoDatabase /> Buy now!
-        </Button>
-      </div>
-      <div>
-        <Button secondary onMouseLeave={handleClick}>
-          <GoCloudDownload />
-          See Deal!
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          Hide Ads!
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded>
-          Test!
-        </Button>
-      </div>
-    </div>
-  );
+  const items = [
+    {
+      id: 0,
+      label: "Can I use React on any project?",
+      content: "Yes, you can use React on any project you want!",
+    },
+    {
+      id: 1,
+      label: "Can I use Javascript on any project?",
+      content: "Yes, you can use Javascript on any project you want!",
+    },
+    {
+      id: 2,
+      label: "Can I use CSS on any project?",
+      content: "Yes, you can use CSS on any project you want!",
+    },
+  ];
+  return <Accordion items={items} />;
 }
 
 export default App;
