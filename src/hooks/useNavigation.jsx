2bfc8f7808129509b900691f377dfa5c0034c8a9
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-04-01 14:49:59 +0300

    Created custom hook for Navigation

---
+++ src/hooks/useNavigation.jsx
@@ -0,0 +1,8 @@
+import NavigationContext from "../context/navigation.jsx";
+import { useContext } from "react";
+
+const useNavigation = () => {
+  return useContext(NavigationContext);
+};
+
+export default useNavigation();
