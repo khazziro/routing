bad26c9d602931a81e65a292f7ec103b81a67472
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-04-01 14:46:30 +0300

    Update Link component to prevent default only for non-modifier clicks

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -5,17 +5,21 @@ const Link = ({ to, children }) => {
   const { navigate } = useContext(NavigationContext);
 
   const handleClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
 
     navigate(to);
   };
-  return <a onClick={handleClick}>{children}</a>;
+  return (
+    <a href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 };
 
 export default Link;
 
-// This component is a reusable link that can be used throughout the application to navigate to a different path.
-// It uses the useContext hook to access the navigate function from the NavigationContext.
-// When a user clicks on the link, the handleClick function is called, which calls the navigate function with the provided to value.
-// This updates the browser's history and navigates to the new path.
-// This component abstracts away the navigation logic and provides a simple and reusable link, making it easier to manage and update links throughout the application.
+// This new implementation improves the user experience by preserving expected browser behavior
+// when clicking links with modifiers while still providing correct behavior for regular clicks.
