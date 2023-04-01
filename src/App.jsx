e63473799494392a71417a8955da849d84b4c273
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-04-01 14:41:26 +0300

    Add Route component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Link from "./components/Link.jsx";
+import Route from "./components/Route.jsx";
+import AccordionPage from "./pages/AccordionPage.jsx";
+import DropdownPage from "./pages/DropdownPage.jsx";
 
 function App() {
   return (
     <div>
       <Link to="/accordion">Go to Accordion</Link>
       <Link to="/dropdown">Go to Dropdown</Link>
+      <div>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
+      </div>
     </div>
   );
 }
---
+++ src/components/Route.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import NavigationContext from "../context/navigation.jsx";
+
+const Route = ({ path, children }) => {
+  const { currentPath } = useContext(NavigationContext);
+  if (path === currentPath) {
+    return children;
+  }
+
+  return null;
+};
+
+export default Route;
+
+// This component is a simple React component that renders its children only when the current path matches the specified path.
+// It uses the useContext hook to access the currentPath variable from the NavigationContext.
+// If the specified path matches the currentPath, the component returns its children. If not, it returns null.
