308da2a9a65dc11f877b24a71220b90e5124c39a
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-04-04 13:18:20 +0300

    Removed hardcode from the table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,12 +1,16 @@
-const Table = ({ data }) => {
+const Table = ({ data, config }) => {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+
   const renderedRows = data.map((fruit) => {
     return (
       <tr className="border-b" key={fruit.name}>
-        <td className="p-3">{fruit.name}</td>
+        <td className="p-3">{config[0].render(fruit)}</td>
         <td className="p-3">
-          <div className={`p-3 m-2 ${fruit.color}`}></div>
+          <div className={`p-3 m-2 ${config[1].render(fruit)}`}></div>
         </td>
-        <td className="p-3">{fruit.score}</td>
+        <td className="p-3">{config[2].render(fruit)}</td>
       </tr>
     );
   });
@@ -14,11 +18,7 @@ const Table = ({ data }) => {
   return (
     <table className="table-auto border-spacing-2">
       <thead>
-        <tr className="border-b-2">
-          <th>Fruit</th>
-          <th>Color</th>
-          <th>Score</th>
-        </tr>
+        <tr className="border-b-2">{renderedHeaders}</tr>
       </thead>
       <tbody>{renderedRows}</tbody>
     </table>
--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -7,9 +7,16 @@ const TablePage = () => {
     { name: "Banana", color: "bg-yellow-500", score: 1 },
     { name: "Lime", color: "bg-green-500", score: 4 },
   ];
+
+  const config = [
+    { label: "Name", render: (fruit) => fruit.name },
+    { label: "Color", render: (fruit) => fruit.color },
+    { label: "Score", render: (fruit) => fruit.score },
+  ];
+
   return (
     <div>
-      <Table data={data} />
+      <Table data={data} config={config} />
     </div>
   );
 };
